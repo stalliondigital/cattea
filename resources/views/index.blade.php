a45c8810c9b18c2a78d4c4bfe379c6a2b9a3fce0
Keegan George <kgeorge13@gmail.com>
2021-03-29 18:14:50 -0700

    Process cards in progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-    {{-- <h1>Test Content for hello</h1> --}}
-
     <!-- Slider main container -->
     <div class="swiper-box">
         <div class="swiper-container">
@@ -33,6 +31,63 @@
                 <div class="swiper-pagination"></div>
                 <div class="swiper-button-next"><i class="fas fa-long-arrow-alt-right"></i></div>
             </nav>
+        </div>
+    </div>
+
+    <div class="mt-10 ml-20 mr-10 process-section">
+        <h2 class="section-title">Our Process</h2>
+        <h4 class="section-subtitle">4 Easy Steps</h4>
+        <div class="process-cards">
+            <div class="card process-card">
+                <div class="process-card-header">
+                    <img src="{{ asset('img/icons/calendar.svg') }}" alt="">
+                    <h5>Book</h5>
+                    <h6>01</h6>
+                </div>
+
+                <div class="process-card-content">
+                    <p>Use our <a href="{{ route('contact') }}">contact form</a> to get in touch with us and book an
+                        appointment. Setup a time that is most convenient for your situation.</p>
+                </div>
+            </div>
+
+            <div class="card process-card">
+                <div class="process-card-header">
+                    <img src="{{ asset('img/icons/camera.svg') }}" alt="">
+                    <h5>Shoot</h5>
+                    <h6>02</h6>
+                </div>
+
+                <div class="process-card-content">
+                    <p>We will take professional photographs for you, capturing the best angles you desire.</p>
+                </div>
+            </div>
+
+            <div class="card process-card">
+                <div class="process-card-header">
+                    <img src="{{ asset('img/icons/settings.svg') }}" alt="">
+                    <h5>Edit</h5>
+                    <h6>03</h6>
+                </div>
+
+                <div class="process-card-content">
+                    <p>Next, we make fine tweaks, implement color grading, and add those final touches that make your image
+                        look stunning!</p>
+                </div>
+            </div>
+
+            <div class="card process-card">
+                <div class="process-card-header">
+                    <img src="{{ asset('img/icons/send.svg') }}" alt="">
+                    <h5>Deliver</h5>
+                    <h6>04</h6>
+                </div>
+
+                <div class="process-card-content">
+                    <p>Delivery time! Visit our online image gallery to download your top quality and high resolution
+                        photos.</p>
+                </div>
+            </div>
         </div>
     </div>
 
