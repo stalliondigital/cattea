85113845c6d6c441cc2808ab5c627397cdc6bb43
Keegan George <kgeorge13@gmail.com>
2021-04-02 18:35:07 -0700

    Add favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#038cba">
+    <meta name="apple-mobile-web-app-title" content="Cattea Artistry">
+    <meta name="application-name" content="Cattea Artistry">
+    <meta name="msapplication-TileColor" content="#f3f4f7">
+    <meta name="theme-color" content="#ffffff">
+
+
     <title>{{ config('app.name', 'Cattea Artistry') }}</title>
 
     <!-- Fonts -->
